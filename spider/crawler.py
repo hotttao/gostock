3aac0d9cd08d32329fde9d17109945299ed8c443
WardSong <1556824234@qq.com>
2020-05-16 00:22:10 +0800

    获取沪港通深港通主成分股

--- spider/crawler.py
+++ spider/crawler.py
@@ -8,6 +8,7 @@ from db_tools.mysql_db import MySQLDB
 
 TABLE_STOCK_BASIC = os.getenv('TABLE_STOCK_BASIC')
 TABLE_STOCK_DAILY = os.getenv('TABLE_STOCK_DAILY')
+TABLE_HS_CONST = os.getenv('TABLE_HS_CONST')
 SQL_QUERY_TS_CODE = "SELECT ts_code from {table} wHERE name='{name}';"
 SQL_QUERY_DAILY_EXISTS = "SELECT max(trade_date) as trade_date FROM {table} WHERE ts_code='{ts_code}';"
 
@@ -17,13 +18,25 @@ class Crawler:
         self.mysql_obj = mysql_obj
         self.stock_api = stock_api
 
-    def crawler_stock_info(self):
+    def crawler_stock_info(self, start=1):
+        """
+        查询当前所有正常上市交易的股票列表
+        :return:
+        """
         df_info = self.stock_api.get_stock_basic()
-        df_info['id'] = df_info.index + 1
-        self.mysql_obj.insert(df_info, 'stock_basic')
+        df_info['id'] = df_info.index + start
+        self.mysql_obj.insert(df_info, TABLE_STOCK_BASIC)
         self.mysql_obj.alter_table(TABLE_STOCK_BASIC)
+        return df_info['id'].max() + 1
 
     def crawler_stock_data(self, name=None, ts_code=None, start_date=None, end_date=None):
+        """
+        :param name:
+        :param ts_code:
+        :param start_date:
+        :param end_date:
+        :return: 查询股票交易数据
+        """
         if not ts_code and name:
             sql_code = SQL_QUERY_TS_CODE.format(table=TABLE_STOCK_BASIC,
                                                 name=name)
@@ -39,8 +52,23 @@ class Crawler:
         if not df_daily.empty:
             print("insert %s from %s to today" % (name, start_date))
             self.mysql_obj.insert(df_daily)
+    
+    def crawler_hs_const(self, hs_type='SH', start=1):
+        """
+        获取沪港通，深港通成分股
+        """
+        df_hs = self.stock_api.get_hs_const(hs_type)
+        df_hs['id'] = df_hs.index + start
+        self.mysql_obj.insert(df_hs, TABLE_HS_CONST)
+        if start == 1:
+            self.mysql_obj.alter_table(TABLE_HS_CONST)
+        return df_hs['id'].max() + 1
+
 
     def init(self):
+        # 创建 hs_const 成份股
+        start_id = self.crawler_hs_const('SH')
+        self.crawler_hs_const('SZ', start_id)
         # 创建 stock_basic 表
         self.crawler_stock_info()
         # 创建 stock_daily 表
@@ -54,8 +82,8 @@ def main():
     mysql_obj = MySQLDB()
     stock_api = tushare_api.StockApi()
     crawler_obj = Crawler(mysql_obj, stock_api)
-    crawler_obj.crawler_stock_data(name='闻泰科技')
-    # crawler_obj.init()
+    # crawler_obj.crawler_stock_data(name='闻泰科技')
+    crawler_obj.init()
 
 
 if __name__ == "__main__":
