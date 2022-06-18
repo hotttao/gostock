f69e07928920081bc2cac2264340536840637b72
tsong <tsong@163.com>
2022-06-18 15:29:14 +0800

    初始化connect

---
+++ connector/noxfile.py
@@ -0,0 +1,21 @@
+
+import os
+import nox
+
+
+DEFAULT_PYTHON_VERSIONS = ["3.8"]
+PYTHON_VERSIONS = os.environ.get(
+    "NOX_PYTHON_VERSIONS", ",".join(DEFAULT_PYTHON_VERSIONS)
+).split(",")
+
+SILENT = True
+
+def deps(session, editable_install=False):
+    session.install("--upgrade", "setuptools", "pip", silent=SILENT)
+    session.run("pip", "install", "-r", "requirements/dev.txt", silent=SILENT)
+    session.run("pip", "install", "-r", "requirements/dev.txt", silent=SILENT)
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def connector(session):
+    deps(session)
