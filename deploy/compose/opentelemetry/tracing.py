29e92d4dbffe6eba954358ffafdd7062e2c2de97
tsong <tsong@163.com>
2022-08-08 08:26:16 +0800

    opentelemetry 基础用法测试

---
+++ deploy/compose/opentelemetry/tracing.py
@@ -0,0 +1,19 @@
+# tracing.py
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleSpanProcessor(ConsoleSpanExporter())
+)
+
+tracer = trace.get_tracer(__name__)
+
+with tracer.start_as_current_span("foo"):
+    with tracer.start_as_current_span("bar"):
+        with tracer.start_as_current_span("baz"):
+            print("Hello world from OpenTelemetry Python!")
